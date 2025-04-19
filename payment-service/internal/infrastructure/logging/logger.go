e00d3b8b3aa1ad809183d02b714f05acba00c6ff
sandy <sandy.hasanudin@yahoo.com>
2025-04-19 22:49:53 +0700

    Create logger handler for payment

---
+++ payment-service/internal/infrastructure/logging/logger.go
@@ -0,0 +1,36 @@
+package logging
+
+import (
+	"log/slog"
+	"os"
+	"payment-service/internal/config"
+	"strings"
+
+	"github.com/go-chi/traceid"
+)
+
+func NewLogger(cfg config.LoggerConfig) *slog.Logger {
+	var level slog.Level
+	switch strings.ToLower(cfg.Level) {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{
+		Level:     level,
+		AddSource: level == slog.LevelDebug,
+	}
+	handler := slog.NewJSONHandler(os.Stdout, opts)
+	handler = traceid.LogHandler(handler)
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+	return logger
+}
